61366fc9aad2ae5fa7367b1823003934ae185256
Tony Baker <this.tonybaker@gmail.com>
2019-02-14 19:58:45 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  born_hash = {"Tony's Birthday" => "February"}
   
 end
 
