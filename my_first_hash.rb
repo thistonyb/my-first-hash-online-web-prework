a872b3eb0a9fe048c80e8673f12983a31451c450
Tony Baker <this.tonybaker@gmail.com>
2019-02-14 20:03:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   born_hash = {"Tony's Birthday" => "February"}
-
 end
 
 
@@ -9,8 +8,11 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-
-
+  the_manifest = {}
+  the_manifest["whale bone corset"] = 5
+  the_manifest["porcelain vases"] = 2
+  the_manifest["oil paintings"] = 3
+  return the_manifest
 end
 
 def retrieval
